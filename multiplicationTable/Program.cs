fed18e51b916e25a16a7ef975e6397a2972b3ee7
MohamedFathi742 <mofathi742002@gmail.com>
2024-11-24 22:08:04 +0200

    Add project files.

---
+++ multiplicationTable/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace multiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //welcome to user 
+
+            Console.WriteLine("welcome to Multiplication Table app ");
+            // ask user to enter value
+            //Console.WriteLine("please enter number");
+            //// read value
+            //byte number = 0;
+            //bool isConverted = byte.TryParse(Console.ReadLine(), out number);
+            //if (!isConverted) { Console.WriteLine("please inter valid number "); return; }
+            //else
+
+            //{
+
+            for (int k = 1; k < 11; k++)
+            {
+                Console.WriteLine($"Multiplication Table {k} ");
+
+                for (int i = 1; i < 11; i++)
+                {
+                    Console.WriteLine($"{k}*{i} = {k * i}");
+                }
+            }
+
+        }
+    }
+}
